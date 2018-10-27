634852a9d0386019deeb15fb36ffde4d6072130f
liangmingwei-idea <85713534@qq.com>
2018-10-27 22:24:45 +0800

    添加初始化

--- app/src/main/java/com/integrate/mingweidev/MainActivity.java
+++ app/src/main/java/com/integrate/mingweidev/MainActivity.java
@@ -5,11 +5,21 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    int a = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initData();
+        initListener();
+    }
+
+    private void initListener() {
+    }
+
+    private void initData() {
+    }
+
+    private void initView() {
     }
 }
