dbb639ccf4f352216fe2833630ca8d3c62729c78
liangmingwei-idea <85713534@qq.com>
2018-10-28 11:19:10 +0800

    delete init

--- app/src/main/java/com/integrate/mingweidev/MainActivity.java
+++ app/src/main/java/com/integrate/mingweidev/MainActivity.java
@@ -2,24 +2,15 @@ package com.integrate.mingweidev;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv_tile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
-        initData();
-        initListener();
     }
 
-    private void initListener() {
-    }
-
-    private void initData() {
-    }
-
-    private void initView() {
-    }
 }
